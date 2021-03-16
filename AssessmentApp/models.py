c189227c25acc293e60a9b78552c941374ceb5dd
Rakin <RakinChow@gmail.com>
2021-03-16 15:57:05 +0000

    Added Assessesment model

--- AssessmentApp/models.py
+++ AssessmentApp/models.py
@@ -6,3 +6,14 @@ from AssessmentApp import db
 class test(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     name = db.Column(db.String(30))
+
+class AssessmentDetails(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    moduleId = db.Column(db.String(10), nullable=False) #WILL CHANGE TO FORIGNE KEY WHEN TABLE MADE
+    allowedAttemps = db.Column(db.Integer, default = 100)
+    weighting = db.Column(db.Integer , nullable=False)
+    timeLimit =  db.Column(db.DateTime, nullable=False)
+    release =  db.Column(db.DateTime, nullable=False)
+    end =  db.Column(db.DateTime, nullable=False)
+    start =  db.Column(db.DateTime, nullable=False)
+    studentInstructions = db.Column(db.Text())
