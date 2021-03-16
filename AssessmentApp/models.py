ebd53aef4dfbd246ea5eb78c123e54f2fdb3ce9f
Yuanjing Ni <niy4@cardiff.ac.uk>
2021-03-16 23:56:15 +0000

    Added question table 2 model

--- AssessmentApp/models.py
+++ AssessmentApp/models.py
@@ -1,6 +1,7 @@
 # from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from AssessmentApp import db
+from datetime import datetime
 
 
 class test(db.Model):
@@ -17,3 +18,35 @@ class AssessmentDetails(db.Model):
     end =  db.Column(db.DateTime, nullable=False)
     start =  db.Column(db.DateTime, nullable=False)
     studentInstructions = db.Column(db.Text())
+
+class QuestionTypeTwo(db.Model):
+    id = db.Column(db.Integer, Primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False) #Standard answers provided by teaching staff
+    feedback = db.Column(db.Text, nullable=False)
+    level = db.Column(db.Integer, nullable=False) #Question defficulty levels
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
+
+class QuestionTwoAnswer(db.Model):
+    id = db.Column(db.Integer, Primary_key = True)
+    answer = db.Column(db.Text, nullable=False) #Student's answers
+    question_id = db.Column(db.Integer, db.ForeignKey('QuestionTypeTwo.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    parent_id = db.Column(db.Interger, db.ForeignKey('QuestionTwoAnswer.id'), nullable=True)
+    parent = db.relationship('QuestionTwoAnswer', backref='comment_parent', remote_side=id, lazy=True)
+
+class Tag(db.Model): #Question type two tags
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(32), unique=True, index=True, nullable=False)
+    author_id = db.Column(db.Integer,db.ForeignKey('staff.id'),nullable=False)
+    question_id = db.Column(db.Interger, db.ForeignKey('QuestionTypeTwo.id'), nullable=False)
+    parent_id = db.Column(db.Interger, db.ForeignKey('Tag.id'), nullable=True)
+    parent = db.relationship('Tag', backref='Tag_parent', remote_side=id, lazy='dynamic')
+
+
+class point(db.Model): #Question type two points
+    id = db.Column(db.Interger, primary_key=True)
+    point = db.Column(db.Integer, nullable=True)
+    author_id = db.Column(db.Integer,db.ForeignKey('staff.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('QuestionTypeTwo.id'), nullable=False)
