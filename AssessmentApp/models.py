023153b222afd7259e802591571b656ed28a291e
Fin Roy <royfe@cardiff.ac.uk>
2021-03-23 16:45:57 +1000

    Added new models from UML diagram

--- AssessmentApp/models.py
+++ AssessmentApp/models.py
@@ -3,29 +3,76 @@ from flask_sqlalchemy import SQLAlchemy
 from AssessmentApp import db
 from datetime import datetime
 
-
 class test(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     name = db.Column(db.String(30))
 
-class AssessmentDetails(db.Model):
+class user(db.Model):
+    id=db.Column(db.Integer, primary_key = True)
+    last_name = db.Column(db.String(40), unique = False, nullable = False)
+    first_name = db.Column(db.String(40), unique = False, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(60), nullable = False)
+    is_staff = db.Column(db.Boolean, default = False, nullable = False)
+
+class modules(db.Model): #statistics
+    id = db.Column(db.Integer , primary_key = True) 
+    module_id = db.Column(db.String(10), unique = True, nullable = False)
+    module_name = db.Column(db.String(40), nullable = False)
+    module_leader = db.Column(db.String(30))
+
+class modules_enrolment(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    module_id = db.Column(db.Integer, db.ForeignKey('modules.id'))
+
+class assessment_details(db.Model):
     id = db.Column(db.Integer , primary_key = True)
-    moduleId = db.Column(db.String(10), nullable=False) #WILL CHANGE TO FORIGNE KEY WHEN TABLE MADE
-    allowedAttemps = db.Column(db.Integer, default = 100)
+    module_id = db.Column(db.Integer, db.ForeignKey('modules.id')) 
+    allowed_attemps = db.Column(db.Integer, default = 100)
     weighting = db.Column(db.Integer , nullable=False)
     timeLimit =  db.Column(db.DateTime, nullable=False)
     release =  db.Column(db.DateTime, nullable=False)
     end =  db.Column(db.DateTime, nullable=False)
     start =  db.Column(db.DateTime, nullable=False)
-    studentInstructions = db.Column(db.Text())
+    student_instructions = db.Column(db.Text())
 
-class Modules(db.Model): #statistics
-    id = db.Column(db.Integer , primary_key = True) 
-    module_id = db.Column(db.String(10))
-    module_name = db.Column(db.String(40))
-    module_leader = db.Column(db.String(30))
+class assessment_results(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    assessment_id = db.Column(db.Integer, db.ForeignKey('assessment_details.id'))
+    attempt_number = db.Column(db.Integer, nullable = False)
+    grade = db.Column(db.Integer, nullable = False)
+    date_completed = db.Column(db.DateTime, nullable=False)
 
-class Student(db.Model): #Student Info
+class assessment_questions(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    assessment_id = db.Column(db.Integer, db.ForeignKey('assessment_details.id'))
+    question_type = db.Column(db.Boolean, default = False, nullable = False)
+
+class question(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    value = db.Column(db.Integer, nullable = False)
+    difficulty = db.Column(db.Integer, nullable = False)
+    question = db.Column(db.Text(), nullable = False)
+    answer_1 = db.Column(db.Text(), nullable = True)
+    answer_2 = db.Column(db.Text(), nullable = True)
+    answer_3 = db.Column(db.Text(), nullable = True)
+    answer_4 = db.Column(db.Text(), nullable = True)
+    correct_answer = db.Column(db.Integer, nullable = True)
+    type_2_answer = db.Column(db.Boolean, nullable = True)
+    correct_feedback = db.Column(db.Text(), nullable = False)
+    incorrect_feedback = db.Column(db.Text(), nullable = False)
+
+class survey(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    assessment_id = db.Column(db.Integer, db.ForeignKey('assessment_details.id'))
+    question_1 = db.Column(db.Integer, nullable = False)
+    question_2 = db.Column(db.Integer, nullable = False)
+    question_3 = db.Column(db.Integer, nullable = False)
+    question_4 = db.Column(db.Integer, nullable = False)
+
+"""class Student(db.Model): #Student Info
     id = db.Column(db.Integer, primary_key=True)
     modules = db.relationship('Modules', backref='module_parent', lazy='dynamic')
     # Student name
@@ -39,15 +86,7 @@ class Staff(db.Model): # Staff Info
     modules = db.relationship('Modules', backref='instructor', lazy='dynamic')
     # Same with staff
 
-class Assessment_Results(db.Model):
-    attempt_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    student_id = db.relationship('Student', backref='Student', lazy='dynamic')
-    assessment_id = db.relationship('AssessmentDetails', backref='Modules', lazy='dynamic')
-    attempt_number = db.Column(db.Integer)
-    grade = db.Column(db.Integer)
-    date_completed = db.Column(db.DateTime, nullable=False)
-    no_of_attempts = db.Column(db.Integer)
-    no_of_attempts = db.Column(db.Integer)
+
 
 class QuestionTypeTwo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -60,7 +99,7 @@ class QuestionTypeTwo(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
 
 
-"""
+
 class QuestionTwoAnswer(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     answer = db.Column(db.Text, nullable=False) #Student's answers
