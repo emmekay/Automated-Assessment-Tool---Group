1bba5f7976e4bbe1384db45949ec8b169a3171c6
Fin Roy <royfe@cardiff.ac.uk>
2021-03-23 17:09:28 +1000

    changed requirements to add login and workzeug

--- AssessmentApp/models.py
+++ AssessmentApp/models.py
@@ -2,6 +2,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from AssessmentApp import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class test(db.Model):
     id = db.Column(db.Integer , primary_key = True)
@@ -11,10 +13,29 @@ class user(db.Model):
     id=db.Column(db.Integer, primary_key = True)
     last_name = db.Column(db.String(40), unique = False, nullable = False)
     first_name = db.Column(db.String(40), unique = False, nullable = False)
+    username = db.Column(db.String(15), unique = True, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
     password_hash = db.Column(db.String(128))
     password = db.Column(db.String(60), nullable = False)
     is_staff = db.Column(db.Boolean, default = False, nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}')"
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
 
 class modules(db.Model): #statistics
     id = db.Column(db.Integer , primary_key = True) 
