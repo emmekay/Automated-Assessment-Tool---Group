85734c88a5024b945f805b7c57855d4564f2c118
Ndungu <karanjan@cardiff.ac.uk>
2021-03-24 09:38:33 +0300

    Added My Assessments, Completed Assessments & Assessment Stats pages to routes.py and fixed links

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -9,3 +9,15 @@ def index():
     # testData = test.query.all()
 
     return render_template('index.html')#, test1 = testData)
+
+@app.route('/my_assessments')
+def my_assessments ():
+    return render_template('my_assessments.html')
+
+@app.route('/completed_assessments')
+def completed_assessments ():
+    return render_template('completed_assessments.html')
+
+@app.route('/assessment_statistics')
+def assessment_statistics ():
+    return render_template('assessment_statistics.html')
