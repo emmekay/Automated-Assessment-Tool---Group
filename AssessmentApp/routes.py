ba989499d69602d736be84567fc993fa583e47ed
Rakin <rakinchow@gmail.com>
2021-03-24 12:54:23 +0000

    Assesment page made

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -7,12 +7,27 @@ from AssessmentApp.models import *
 @app.route('/')
 def index():
     # testData = test.query.all()
+    user1 = user.query.filter_by(id=1).first()
+
+    print("Bool test")
+    print(user1.is_staff)
 
     return render_template('index.html')#, test1 = testData)
 
 
 @app.route('/addAss')
 def addAss():
-    # testData = test.query.all()
 
-    return render_template('AssessmentDetails.html')#, test1 = testData)
+    return render_template('AssessmentDetails.html')
+
+@app.route('/assessment/<int:id>')
+def Ass(id):
+
+    ass = assessment_details.query.filter_by(id=id).first()
+
+
+    questionIds = assessment_questions.query.filter_by(assessment_id=id).all()
+
+    assQuestions = [ question.query.filter_by(id=q.id).first() for q in questionIds]
+
+    return render_template('UndertakeAss.html',  assQuestions =assQuestions, ass = ass)
