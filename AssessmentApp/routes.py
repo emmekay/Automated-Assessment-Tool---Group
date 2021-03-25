029426d91f44db5c09e033b9049c538745c6c7d9
Rakin <rakinchow@gmail.com>
2021-03-25 08:12:40 +0000

    slight changes to routes

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -29,22 +29,22 @@ def Ass(id):
     return render_template('UndertakeAss.html',  assQuestions =assQuestions, ass = ass)
 
 
-# @app.route("/login",methods=['GET','POST'])
-# def login():
-#   form = LoginForm()
-#   if form.validate_on_submit():
-#     user = User.query.filter_by(email=form.email.data).first()
-#     if user is not None and user.verify_password(form.password.data):
-#       login_user(user)
-#       flash('Login successful!')
-#       return redirect(url_for('home'))
-#     flash('Invalid email address or password.')
-#
-#     return render_template('login.html',form=form)
-#
-#   return render_template('login.html',title='Login',form=form)
-#
-# @app.route("/logout")
-# def logout():
-#   logout_user()
-#   return redirect(url_for('login'))
+@app.route("/login",methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user is not None and user.verify_password(form.password.data):
+      login_user(user)
+      flash('Login successful!')
+      return redirect(url_for('home'))
+    flash('Invalid email address or password.')
+
+    return render_template('login.html',form=form)
+
+  return render_template('login.html',title='Login',form=form)
+
+@app.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for('login'))
