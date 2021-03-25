443791e4f3e435f36043a794e0ed0e9fa5f7f897
Rakin <rakinchow@gmail.com>
2021-03-25 08:09:55 +0000

    Temp fixed error by commenting out area

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -24,27 +24,27 @@ def Ass(id):
 
     questionIds = assessment_questions.query.filter_by(assessment_id=id).all()
 
-    assQuestions = [ question.query.filter_by(id=q.id).first() for q in questionIds]
+    assQuestions = [ question.query.filter_by(id=q.question_id).first() for q in questionIds]
 
     return render_template('UndertakeAss.html',  assQuestions =assQuestions, ass = ass)
 
 
-@app.route("/login",methods=['GET','POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
-    if user is not None and user.verify_password(form.password.data):
-      login_user(user)
-      flash('Login successful!')
-      return redirect(url_for('home'))
-    flash('Invalid email address or password.')
-
-    return render_template('login.html',form=form)
-
-  return render_template('login.html',title='Login',form=form)
-
-@app.route("/logout")
-def logout():
-  logout_user()
-  return redirect(url_for('login'))
+# @app.route("/login",methods=['GET','POST'])
+# def login():
+#   form = LoginForm()
+#   if form.validate_on_submit():
+#     user = User.query.filter_by(email=form.email.data).first()
+#     if user is not None and user.verify_password(form.password.data):
+#       login_user(user)
+#       flash('Login successful!')
+#       return redirect(url_for('home'))
+#     flash('Invalid email address or password.')
+#
+#     return render_template('login.html',form=form)
+#
+#   return render_template('login.html',title='Login',form=form)
+#
+# @app.route("/logout")
+# def logout():
+#   logout_user()
+#   return redirect(url_for('login'))
