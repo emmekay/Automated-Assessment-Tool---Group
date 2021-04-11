60fa177d892dbcafb1a6e6690e9a924c258cdb5b
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-04-11 17:15:59 +0000

    Update routes.py to match FinBranch

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -1,46 +1,81 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
-from AssessmentApp import app
-from AssessmentApp.models import *
-from AssessmentApp.forms import LoginForm
 from flask_login import login_user, logout_user, login_required, current_user
+from AssessmentApp import app, login_manager
+from AssessmentApp.forms import LoginForm
+from AssessmentApp.models import *
 
-@app.route('/')
-def index():
-    # testData = test.query.all()
-
-
-    return render_template('index.html')#, test1 = testData)
-
-
-@app.route('/addAss')
-def addAss():
-    return render_template('AssessmentDetails.html')
-
-@app.route('/assessment/<int:id>')
-def Ass(id):
-
-    ass = assessment_details.query.filter_by(id=id).first()
-
-
-    questionIds = assessment_questions.query.filter_by(assessment_id=id).all()
-
-    assQuestions = [ question.query.filter_by(id=q.question_id).first() for q in questionIds]
-
-    return render_template('UndertakeAss.html',  assQuestions =assQuestions, ass = ass)
-
-
-@app.route("/login",methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
+    User = user.query.filter_by(email=form.email.data).first()
+    #if User is not None and User.verify_password(form.password.data):
+    login_user(User)
+    flash('Login successful!')
+    return redirect(url_for('home'))
+    #flash('Invalid email address or password.')
+    
+    #return render_template('login.html',form=form)
+
+  return render_template('login.html',title='Login',form=form)
+    # testData = test.query.all()
+
+@app.route('/home',methods=['GET','POST'])
+def home():
+  return render_template('index.html')
+
+@app.route('/addAss')
+def addAss():
+    # testData = test.query.all()
+
+    return render_template('AssessmentDetails.html')#, test1 = testData)
+
+"""@app.route("/login",methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = user.query.filter_by(email=form.email.data).first()
     if user is not None and user.verify_password(form.password.data):
       login_user(user)
       flash('Login successful!')
       return redirect(url_for('home'))
     flash('Invalid email address or password.')
+    
+    return render_template('login.html',form=form)
+
+  return render_template('login.html',title='Login',form=form)
+"""
+@app.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for('login'))
+
+@app.route("/view-modules")
+def view_modules():
+  #enrolled = modules_enrolment.query.all()
+  Modules = modules.query.all()
+  return render_template('view_modules.html', Modules=Modules)
+
+@app.route("/view-assessments/<int:module_id>")
+def view_assessments(module_id):
+  assess = assessment_details.query.filter(assessment_details.module_id==module_id)
+  return render_template('view_assessments.html', assess=assess)
+
+@app.route("/edit-assessments/<int:assess_id>")
+def edit_assessment(assess_id):
+  assess = assessment_details.query.filter(assessment_details.id==assess_id)
+  return render_template('edit_assessment.html', assess=assess)
+
+"""@app.route("/delete-assessments/<int:assess_id>")
+def delete_assessment(assess_id,module_id):
+  assess = assessment_details.query.filter(assessment_details.id==assess_id)
+  db.session.delete(assess)
+  db.session.commit()
+  assess = assessment_details.query.filter(assessment_details.module_id==module_id)
+  return render_template('view_assessments_staff.html',assess=assess)"""
+
 
     return render_template('login.html',form=form)
 
