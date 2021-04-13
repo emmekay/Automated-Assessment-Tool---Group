f03748b6bcf11df7211672ba34f04050082e778a
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-04-13 14:04:58 +0100

    update init.py to remove blog references

--- AssessmentApp/__init__.py
+++ AssessmentApp/__init__.py
@@ -1,6 +1,8 @@
+from AssessmentApp import routes
 from flask import Flask
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
 
 
 app = Flask(__name__)
@@ -8,20 +10,21 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'dqOTxaMwwDDEzBSk1PE_5zeJ_ow'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///primaryDatabase.db'
 # app.config["SECRET_KEY"] = 'dqOTxaMwwDDEzBSk1PE_5zeJ_ow'
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app) # EK commeted out - was listed twice 
 
-from AssessmentApp import routes
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-from blog import routes
 
-from flask_admin import Admin
-from blog.views import AdminView
-from blog.models import User, Post, Comment
-admin = Admin(app,name='Admin panel', template_mode='bootstrap3')
-admin.add_view(AdminView(User, db.session))
-admin.add_view(AdminView(Post, db.session))
-admin.add_view(AdminView(Comment, db.session))
+# EK commented out - Shouldn't have blog references
+#from blog import routes
+
+#from flask_admin import Admin
+#from blog.views import AdminView
+#from blog.models import User, Post, Comment
+#admin = Admin(app,name='Admin panel', template_mode='bootstrap3')
+#admin.add_view(AdminView(User, db.session))
+#admin.add_view(AdminView(Post, db.session))
+#admin.add_view(AdminView(Comment, db.session))
