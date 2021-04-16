e30f517405386c266ab345d86af2f29ceaf1192a
Fin Roy <royfe@cardiff.ac.uk>
2021-04-16 21:16:28 +1000

    merging with fins routes and register

--- AssessmentApp/forms.py
+++ AssessmentApp/forms.py
@@ -1,26 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, Form, SelectField
+from wtforms import StringField, PasswordField, SubmitField, Form, SelectField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, InputRequired, Regexp
 from AssessmentApp.models import user
 
-"""class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    firstname = StringField('First Name', validators=[DataRequired(), Length(max = 40)])
-    lastname = StringField('Last Name', validators=[DataRequired(), Length(max = 40)])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max = 40)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max = 40)])
     password = PasswordField('Password',validators=[DataRequired(),Regexp('^.{6,8}$',message='Your password should be between 6 and 8 characters long.')])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password', message='Passwords do not match.')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
+        User = user.query.filter_by(username=username.data).first()
+        if User is not None:
             raise ValidationError('Username already exists. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Email is already asocciated with another account, please use another.')"""
+        User = user.query.filter_by(email=email.data).first()
+        if User is not None:
+            raise ValidationError('Email is already asocciated with another account, please use another.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
