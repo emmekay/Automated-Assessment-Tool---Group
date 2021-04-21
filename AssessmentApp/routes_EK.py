360aca055b7ec585a941a07a5ba0066544625013
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-04-21 10:42:05 +0100

    survey

--- AssessmentApp/routes_EK.py
+++ AssessmentApp/routes_EK.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+
+
 #from datetime import datetime
 
-from AssessmentApp import app
+from AssessmentApp import app, db
+from AssessmentApp import models
 from AssessmentApp.models import *
+from AssessmentApp.forms import *
 
 
-@app.route("/survey")
+"""@app.route("/survey")
 def survey():
     #print("Total number of surveys is", survey.query.count())
-    return render_template('survey.html', title='Assessment Completed')
+    return render_template('survey.html', title='Assessment Completed')"""
+
+
+@app.route("/survey", methods=['GET', 'POST'])
+def surveyinfo():
+    form = Survey()
+    if form.validate_on_submit():
+        user = Survey(user=models.user_id.data, assessment_id=models.assessment_id.data, question_1=form.question_1.data, question_2=form.question_2.data, question_3=form.question_3.data, question_4=form.question_4.data, question_5=form.question_5.data, question_6=form.question_6.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Survey Submitted')
+        return redirect(url_for('index.html'))
+    return render_template('survey.html', title='Survey', form=form)
 
 
 @app.route("/staffaccount")  # EK
