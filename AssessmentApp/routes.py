41b0493fa8fddcffb25d1ed2807b7109554921f1
Fin Roy <royfe@cardiff.ac.uk>
2021-04-23 23:39:58 +1000

    Added edit and delete functionality to assessments

--- AssessmentApp/routes.py
+++ AssessmentApp/routes.py
@@ -89,11 +89,36 @@ def view_assessments(module_id):
 
   return render_template('view_assessments.html', assess=assess, id = module_id )
 
-@app.route("/edit-assessments/<int:assess_id>")
+@app.route("/edit-assessments/<int:assess_id>", methods = ["GET", "POST"])
 def edit_assessment(assess_id):
   assess = assessment_details.query.filter(assessment_details.id==assess_id)
-  return render_template('edit_assessment.html', assess=assess)
+  ass = assessment_details.query.filter(assessment_details.id==assess_id).first()
+  module = modules.query.filter(modules.id==assessment_details.module_id)
+  mod = modules.query.filter(modules.id==assessment_details.module_id).first()
+  if request.method == "POST":
+      #assess.module_id = module.id
+      ass.assessment_type = bool(request.form['assType'])
+      ass.assessment_name = request.form['assTitle']
+      ass.time_limit = datetime.strptime(request.form['assTime'], '%Y-%m-%d %H:%M:%S')
+      ass.start_date = datetime.strptime(request.form['assStart'], '%Y-%m-%d %H:%M:%S')
+      ass.end_date = datetime.strptime(request.form['assEnd'], '%Y-%m-%d %H:%M:%S')
+      ass.release = datetime.strptime(request.form['assRel'], '%Y-%m-%d %H:%M:%S')
+      ass.weighting = int(request.form['assWeight'])
+      ass.allowed_attemps = int(request.form['assAttemps'])
+      ass.assessment_instructions = request.form['assInstruc']
 
+      db.session.commit()
+      return redirect(url_for('view_assessments', module_id = mod.id))
+    
+  return render_template('edit_assessment.html', assess=assess, module=module)
+
+@app.route("/delete-assessments/<int:assess_id>")
+def delete_assessment(assess_id):
+  assess = assessment_details.query.filter(assessment_details.id==assess_id).first()
+  mod = modules.query.filter(modules.id==assessment_details.module_id).first()
+  db.session.delete(assess)
+  db.session.commit()
+  return redirect(url_for('view_assessments', module_id = mod.id))
 
 
 @app.route('/my_assessments')
@@ -118,14 +143,6 @@ def register():
     flash('Registration Succesful, Please login now.')
     return redirect(url_for('login'))
   return render_template('register.html', title='Register', form=form)
-
-"""@app.route("/delete-assessments/<int:assess_id>")
-def delete_assessment(assess_id,module_id):
-  assess = assessment_details.query.filter(assessment_details.id==assess_id)
-  db.session.delete(assess)
-  db.session.commit()
-  assess = assessment_details.query.filter(assessment_details.module_id==module_id)
-  return render_template('view_assessments_staff.html',assess=assess)"""
 
 
  # return render_template('login.html',form=form)
