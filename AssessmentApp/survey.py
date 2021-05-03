ef2d1641f35ce8d0545496a07c053d1e9a0abea4
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-05-03 11:17:14 +0100

    Update files

--- AssessmentApp/survey.py
+++ AssessmentApp/survey.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+"""from flask import Flask, render_template, request, redirect, url_for, flash
 #from flask_login import login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 #from datetime import datetime
